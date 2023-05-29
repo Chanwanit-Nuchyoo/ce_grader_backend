e53e97d1614d3851b9f34a5480328e5122509008
Chanwanit Nuchyoo <chanwanit@primo.mobi>
2023-05-29 23:18:50 +0700

    first go backend setup

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	LoadEnvVariables()
+	//database.Init()
+}
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
